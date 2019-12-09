2cd5fea7eef0824a0cd5b9f3869b9e1ecbf07d88
James Lenahan <james.k.lenahan@gmail.com>
2019-12-09 16:19:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,4 +17,20 @@ class Song
   def self.count
     @@count
   end
-end
+  def self.genres
+    @@genres.uniq!
+  end
+  def self.artists
+    @@artists.uniq!
+  end
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
