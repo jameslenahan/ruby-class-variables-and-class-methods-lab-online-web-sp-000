bfedef7f557d79fd07b17e923d69678b70dcc0e8
James Lenahan <james.k.lenahan@gmail.com>
2019-12-09 16:16:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,4 +8,12 @@ class Song
   def initialize(song_name, artist, genre)
     @name = song_name
     @artist = artist
-    
+    @genre = genre 
+    @@count += 1 
+    @@genres << genre
+    @@artists << artist
+  end
+  
+  def self.count
+    @@count
+  end
